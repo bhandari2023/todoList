7beeafdbb0d474ef5ae4f6d6324efa05be8fab14
Pankaj Bhandari <pankajbhandari2010@gmail.com>
2025-05-26 15:58:17 +0530

    todo

--- script.js
+++ script.js
@@ -7,19 +7,46 @@ addButton.onclick = function () {
         const todoItem = document.createElement('li');
         todoItem.textContent = todoText;
         const deleteButton = document.createElement('button');
-        deleteButton.textContent = '❌';
-        deleteButton.className = 'delete-button';
-        deleteButton.onclick = function () {
+        deleteButton.textContent = 'X';
+        deleteButton.className = "delete-button"
+        deleteButton.onClick = function () {
             todoList.removeChild(todoItem);
         }
         todoItem.appendChild(deleteButton);
         todoList.appendChild(todoItem);
-        todoInput.value = ''; // Clear input field after adding
-
-        todoInput.focus(); // Focus back on input field
-    }
-    else {
-        alert('Please enter a todo item.');
+        console.log(todoItem)
+        todoInput.value = "";
+    } else {
+        alert("please enter a todo item");
     }
 
-}   
+}
+
+
+
+
+// const todoInput = document.getElementById('todo-input');
+// const todoList = document.getElementById('todo-list');
+// const addButton = document.getElementById('add-todo');
+// addButton.onclick = function () {
+//     const todoText = todoInput.value.trim();
+//     if (todoText) {
+//         const todoItem = document.createElement('li');
+//         todoItem.textContent = todoText;
+//         const deleteButton = document.createElement('button');
+//         deleteButton.textContent = '❌';
+//         deleteButton.className = 'delete-button';
+//         deleteButton.onclick = function () {
+//             todoList.removeChild(todoItem);
+//         }
+//         todoItem.appendChild(deleteButton);
+//         todoList.appendChild(todoItem);
+//         todoInput.value = ''; // Clear input field after adding
+
+//         todoInput.focus(); // Focus back on input field
+//     }
+//     else {
+//         alert('Please enter a todo item.');
+//     }
+
+// }   
